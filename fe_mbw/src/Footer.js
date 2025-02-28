8d559c768922d0216ecb93153ce95e600ebd51e3
Loading... <158498496+Ash-a9236@users.noreply.github.com>
2025-02-28 12:14:19 -0600

    adding Footer file + update gallery

---
+++ fe_mbw/src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+    <footer>
+        <center>
+            <p>made by &#8460elene &#8476ousseau <br />6229734&#64edu.vaniercollege.qc.ca<br /> Created February 2025 &#169</p>
+        </center>
+    </footer>
+    );
+  }
+  
+  export default Footer;
